562a461dfe450b3265f0d5a02aa1cdd908dbc8b8
Jake Turchetta <14jturchetta@gmail.com>
2020-07-09 22:00:00 -0700

    separate navbar aand header

---
+++ client/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import ProfileScreen from './ProfileScreen.jsx';
+
+
+const TopTab = createMaterialTopTabNavigator();
