5b694da55f49be794aea19346ecd5a2baa65e843
Jake Turchetta <14jturchetta@gmail.com>
2020-07-14 23:01:26 -0400

    bottom tab navigator bug fixes -- entirely functional

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
 import Header from './navigation/Header.jsx';
 import TabNavigator from './navigation/TabNavigator.jsx';
-import * as eva from '@eva-design/eva';
-import { mapping, light as lightTheme } from '@eva-design/eva'
-import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+// import * as eva from '@eva-design/eva';
+import { mapping, light } from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry, Layout, Text } from 'react-native-ui-kitten';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 const ApplicationContent = () => (
@@ -23,7 +23,7 @@ export default class App extends React.Component {
     return (
       <Fragment>
         <IconRegistry icons={EvaIconsPack} />
-        <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <ApplicationProvider mapping={mapping} theme={light} >
           <TabNavigator />
         </ApplicationProvider>
       </Fragment>
--- client/components/navigation/TabNavigator.jsx
+++ client/components/navigation/TabNavigator.jsx
@@ -6,13 +6,12 @@ import Fontisto from 'react-native-vector-icons/Fontisto';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import DonateScreen from '../screens/Donate.jsx';
 import VideosScreen from '../screens/Videos.jsx';
 import NewsScreen from '../screens/News.jsx';
 import EventsScreen from '../screens/Events.jsx';
 import ResourcesScreen from '../screens/Resources.jsx';
-import { Icon } from '@ui-kitten/components'
+import { Icon } from 'react-native-ui-kitten';
 
 const BottomTab = createBottomTabNavigator();
 
@@ -30,43 +29,55 @@ export default class TabNavigator extends React.Component {
           initialRouteName="NewsFeed"
           tabBarOptions={{
             showLabel: false,
-            activeBackgroundColor: '#010002'
+            showIcon: true
           }}>
           <BottomTab.Screen
             name="Donate"
             component={DonateScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome5 name="donate" color={'#AA1C0D'} size={size} />
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  name="gift-outline"
+                  width={32}
+                  height={32}
+                  fill={focused ? '#111' : '#939393'} />
               ),
             }} />
           <BottomTab.Screen
             name="Videos"
             component={VideosScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Fontisto name="youtube-play" color={'#F16B0E'} size={size} />
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  name="play-circle-outline"
+                  width={32}
+                  height={32}
+                  fill={focused ? '#111' : '#939393'} />
               ),
             }} />
           <BottomTab.Screen
             name="NewsFeed"
             component={NewsScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome name="newspaper-o" color={'#ECEC0D'} size={size} />
-              ),
-            }} />
-          <BottomTab.Screen
-            name="Events"
-            component={EventsScreen}
-            options={{
-              tabBarIcon: ({ focused }) => {
+              tabBarIcon: ({ focused }) => (
                 <Icon
-                  name="calendar"
+                  name="home-outline"
                   width={32}
                   height={32}
                   fill={focused ? '#111' : '#939393'} />
-              },
+              ),
+            }} />
+            <BottomTab.Screen
+            name="Events"
+            component={EventsScreen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  name="calendar-outline"
+                  width={32}
+                  height={32}
+                  fill={focused ? '#111' : '#939393'} />
+              ),
             }} />
           <BottomTab.Screen
             name="Resources"
@@ -77,8 +88,7 @@ export default class TabNavigator extends React.Component {
                   name="heart-outline"
                   width={32}
                   height={32}
-                  fill={focused ? '#111' : '#939393'}
-                />
+                  fill={focused ? '#111' : '#939393'} />
               ),
             }} />
         </BottomTab.Navigator>
